3a5848ddca9c44090961c0d283970e6e87bc19cd
Huseyin YILDIZ <yildizhuseyin@gmail.com>
2026-04-09 17:02:22 +0300

    Update

---
+++ Manyetik Alan Modelleme/SIM_2D_air_core_motor_v0_2.py
@@ -0,0 +1,202 @@
+# -*- coding: utf-8 -*-
+"""
+Tek bobin ile motor dönme simulasyonu
+
+@author: pc64x
+"""
+from class_bobin import *
+from class_objects import*
+from class_controller import*
+
+
+
+
+# Tanımlamalar ve Ölçüler 
+# Bobin özellikleri 
+a=100.0/1000.0; # bobin yarıçapı (m) 
+N=100.0 # Bobin sargı sayısı  
+I=1.0 # Akım (A)
+
+bobin1=bobin(a,i=I,z0=1*a/5,n=N,alfa=0)
+bobin2=bobin(a,i=I,z0=-1*a/5,n=N,alfa=0)
+bobin3=bobin(a,i=I,z0=1*a/5,n=N,alfa=90)
+bobin4=bobin(a,i=I,z0=-1*a/5,n=N,alfa=90)
+
+# bobin2=bobin(a,i=I,z0=-a/5,n=N)
+# %%  Robot Ayarları ve Tanımlama 
+
+m0=0.4 # mıknatısın manyetik dipol (A*m2)
+M=0.0001 # Ağırlık (kg) 
+J=0.001 # Atalet momenti 
+g=1*9.81 
+b= 0.001 # sürtünme kuvveti 
+z0=0.0;
+dz0=0.0;
+q0=0*np.pi/180;
+dq0=0*np.pi/180;
+
+
+robot1=micro_robot_1D(M,m0) 
+robo_color=(250,0,0)   
+robot1.set_color(robo_color)
+robot1.set_size(0.75/1000)
+x=[0.0,0.0,z0]
+v=[0.0,0.0,0]
+robot1.update_position(x,v)
+q=[q0,0.0,0]
+w=[dq0,0.0,0]
+robot1.update_rotation(q,w)
+
+
+magnet_list=[]
+# magnet listesi ([r, DQ, yon ])
+magnet_list.append([a/6,0,1])
+# magnet_list.append([a/6,180,1])
+# %%
+
+
+
+def get_magnetic_fields_from_list(x,y,z):
+    B=np.zeros([4,])
+    for bobin in ekran.coils:
+        b=bobin.get_magnetic_field_rotated(x,y,z)
+        B=B+b
+    B[3]=np.sqrt(B[0]**2+B[1]**2+B[2]**2)
+    return B
+def get_magnet_vector(mm,tet):
+    magnet_vector=[np.zeros([1,]),mm*np.sin(tet),mm*np.cos(tet)]
+    return np.array(magnet_vector)
+# Simulasyon ayarları 
+
+def get_force(X):
+    
+    for r,dQ,yon in magnet_list: 
+        x=0
+        y=r*np.cos(aci+np.pi*dQ/180)
+        z=r*np.sin(aci+np.pi*dQ/180)
+        R=np.array([x,y,z])
+        BB,FF,TT=get_force_with_magnet(x, y, z)
+        T=TT+np.cross(R,FF)
+    return 0,f,T[0]
+
+def get_force_with_magnet(x,y,z,tet):
+    m=get_magnet_vector(m0,tet)
+    d_z=1e-3
+    x0=x
+    y0=y
+    z0=z
+    y0a=y0-d_z
+    y0b=y0+d_z
+    z0a=z0-d_z
+    z0b=z0+d_z
+    B=get_magnetic_fields_from_list(x0,y0,z0)
+    B_za=get_magnetic_fields_from_list(x0,y0,z0a)
+    B_zb=get_magnetic_fields_from_list(x0,y0,z0b)
+    B_ya=get_magnetic_fields_from_list(x0,y0a,z0)
+    B_yb=get_magnetic_fields_from_list(x0,y0b,z0)
+    # B_xa=get_magnetic_fields_from_list(x0a,y0,z0)
+    # B_xb=get_magnetic_fields_from_list(x0b,y0,z0)
+    # dB_dx=(B_xb[:3]-B_xa[:3])/(2*d_z)
+    dB_dy=(B_yb[:3]-B_ya[:3])/(2*d_z)
+    dB_dz=(B_zb[:3]-B_za[:3])/(2*d_z)
+    T=np.array([])
+    force=m0*dBz_dz
+    mm=np.reshape(m,[-1,])
+    BB=np.reshape(B[:3],[-1,])
+    torqe=np.cross(mm, BB)
+    # print(torqe)
+    return B,force,torqe
+
+def dF(t,X,F):
+    # T=m x B
+    # F=m . (Nabla B) 
+    df=np.zeros_like(X)
+    B,Fz,Tx=get_force(X)
+    df[0]=X[1]
+    df[1]=Tx/J-b*X[1]/J
+    return df
+
+
+# %%
+
+
+t0=0; ts=100; dt=0.01
+
+n=int((ts-t0)/dt)
+t=0
+X=np.array([[q0],[dq0]]) #[z,dz]
+
+SIM=simulator(t0,ts,dt,X,sim_type='RK4')
+SIM.set_fcn(dF)
+
+
+
+
+# %% KONTROLCÜ AYARLARI 
+PID_controller=PID_controller_1D(dt,y_ref=a/10) # ♠kontrolcüyü tanımla 
+PID_controller.set_parameters(kp=700) # PID parametrelerini düzenle 
+
+
+
+# %%  SIMULASYON EKRANI VE AYARLARI  
+ekran=pygame_screan_2D(min_X=[-0.1/2,-0.1/2],max_X=[0.1/2,0.1/2]) # Ekranı oluştur 
+bg_color=(220,220,220) 
+ekran.set_backgraun_color(bg_color) # Arka plan rengini ayarla 
+ekran.set_textSize(30) # Yazı fontu büyüklüğünü ayarla 
+
+ekran.add_robot(robot1) # Bir robot geometrisi ekle 
+# ekran.add_robot(robot2) # Bir robot geometrisi ekle 
+
+ekran.add_coil(bobin1)  # Bir bobin ekle 
+ekran.add_coil(bobin2)  # Bir bobin ekle 
+ekran.add_coil(bobin3)  # Bir bobin ekle 
+ekran.add_coil(bobin4)  # Bir bobin ekle # ekran.add_coil(bobin2)  # Bir bobin ekle 
+# %%
+
+running=True
+say=0
+t0=timer.time()
+f=2
+while running:
+    control_current=PID_controller.apply(SIM.X[0,0]) # Kontrolcüden uygulanacak akımı hesaplat 
+    control_current1=40*np.sin(f*SIM.t*2);
+    control_current2=40*np.sin(f*SIM.t*2-np.pi/2);
+    # print(control_current)
+    
+    bobin1.set_current(1*control_current1)
+    bobin2.set_current(1*control_current1)
+    bobin3.set_current(1*control_current2)
+    bobin4.set_current(1*control_current2)
+    SIM.apply() # Simulasyonu çalıştır 
+    
+    # Pozisyon bilgisi sabit 
+    #x=[0.0,0.0,SIM.X[0]]
+    #v=[0.0,0.0,SIM.X[1]]
+    #robot.update_position(x,v)
+    
+    # Dönme bilgisi değişken 
+    q=[SIM.X[0,0],0.0,0.0]
+    dq=[SIM.X[1,0],0.0,0.0]
+    robot1.update_rotation(q,dq)
+    if SIM.adim % 1==0:    
+        magnetic_field,force,torque=get_force(SIM.X) # Manyetik alanları hesapla 
+        running=ekran.draw(n=SIM.adim,t=SIM.get_time(),B=magnetic_field,F=force,T=torque) # Ekranı güncelle 
+        #print(SIM.adim,SIM.get_time())
+    timer.sleep(dt)
+    
+    if SIM.adim==n: 
+        running=False
+t1=timer.time()    
+fark=t1-t0 
+print('geçen süre ',fark)
+ekran.exit_simulation() # Simulasyon ekranından çık 
+
+
+# %% SONUÇ GRAFİKLERİ 
+
+
+plt.plot(SIM.log_T[:,0],SIM.log_X[:,0]*180/np.pi)
+
+# plot_list_points_with_length(1,111,[(Data[:,0],Data[:,1],'-r',2),
+#                                     (SIM2.log_T[:,0],SIM2.log_X[:,0],':k',1),
+#                                     (SIM3.log_T[:,0],SIM3.log_X[:,0],'--g',1)],legend=['RK4','Eu','RK4a'],title='Başlık')
